ea0664933cbce5d7aaab2cd65adae9c1baf88aac
William Silva <williamhenrique.silva98@gmail.com>
2021-02-17 10:10:28 -0300

    Atualização do end-ponit da api

--- src/Api/pexels.js
+++ src/Api/pexels.js
@@ -2,14 +2,14 @@ const BASE_URL = "https://api.pexels.com/v1";
 const API_KEY = "563492ad6f91700001000001ee51c25f13714fdbba56913ea42f2d82";
 
 const PexelsApi = {
-  getCurated: async (max) => {
-    const res = await fetch(`${BASE_URL}/curated?per_page=${max}`, {
+  getCurated: async () => {
+    const res = await fetch(`${BASE_URL}/search?query=nature&per_page=80`, {
       headers: {
         Authorization: API_KEY,
       },
     });
-    console.log(res);
     const data = await res.json();
+    console.log(data);
     return data;
   }
 };
