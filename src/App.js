c6aa4246d9d13b6baa6918017cd4c381bd119fd7
William Silva <williamhenrique.silva98@gmail.com>
2021-02-17 10:20:10 -0300

    Finalização da galeria de fotos v1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './styles.scss'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Gallery from './components/Gallery'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <Gallery />
     </div>
   );
 }
